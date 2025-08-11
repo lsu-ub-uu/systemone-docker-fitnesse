0e4349c2c8b077eeef2b9324a5c777c9db90b368
perebartrolisimo <pere.bartroli@ub.uu.se>
2025-08-11 09:47:33 +0000

    change from bash to sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 : "${BASE_URL:=http://localhost:8080/systemone/}"
 : "${LOGIN_URL:=http://localhost:8080/login/}"
 : "${IDP_LOGIN_URL:=http://localhost:8080/idplogin/}"
 : "${GATEKEEPER_SERVER_URL:=http://localhost:8080/gatekeeperserver/}"
 
-exec java
-  -classpath /fitnesse/fitnesse.jar
-  -DsystemUnderTestUrl="$BASE_URL"
-  -DappTokenVerifierUrl="$LOGIN_URL"
-  -DidpLoginUrl="$IDP_LOGIN_URL"
-  -DgatekeeperServerUrl="$GATEKEEPER_SERVER_URL"
-  fitnesseMain.FitNesseMain
-  -p 8090
+java \
+  -classpath /fitnesse/fitnesse.jar \
+  -DsystemUnderTestUrl="$BASE_URL" \
+  -DappTokenVerifierUrl="$LOGIN_URL" \
+  -DidpLoginUrl="$IDP_LOGIN_URL" \
+  -DgatekeeperServerUrl="$GATEKEEPER_SERVER_URL" \
+  fitnesseMain.FitNesseMain \
+  -p 8090 \
   -a /fitnesse/password.txt
