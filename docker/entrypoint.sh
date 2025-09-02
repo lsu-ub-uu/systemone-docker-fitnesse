c51cd56e59fc263252daa08ca4112a0ca9a190cb
perebartrolisimo <pere.bartroli@ub.uu.se>
2025-09-02 11:39:17 +0000

    continue working

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
+: "${CONTEXT_ROOT_ARG:=someContextRoot}"
 : "${BASE_URL:=http://localhost:8080/systemone/}"
 : "${LOGIN_URL:=http://localhost:8080/login/}"
 : "${IDP_LOGIN_URL:=http://localhost:8080/idplogin/}"
 : "${GATEKEEPER_SERVER_URL:=http://localhost:8080/gatekeeperserver/}"
+
+sed -i "s|{CONTEXT_ROOT}|${CONTEXT_ROOT_ARG}|g" ./plugins.properties
 
 java \
   -classpath /fitnesse/fitnesse.jar \
